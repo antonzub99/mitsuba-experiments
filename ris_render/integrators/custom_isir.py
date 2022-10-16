8fe51a7ac3a324466421740cc0baf182f708cb72
Anton Zubekhin <anton-zubehin@mail.ru>
2022-10-16 13:16:52 +0300

    back to slow isir

--- ris_render/integrators/custom_isir.py
+++ ris_render/integrators/custom_isir.py
@@ -65,18 +65,11 @@ class ISIRIntegrator(MyBaseIntegrator):
             ray, ray_flags=mi.RayFlags.All, coherent=mi.Bool(True), active=active)
         active = active & si.is_valid()
 
-        # Directly visible emission if any
         L += dr.select(active, si.emitter(scene, active).eval(si), mi.Color3f(0))
 
         bsdf: mi.BSDF = si.bsdf(ray)
 
         reservoir = ReservoirISIR(size=sampler.wavefront_size(), n_proposals=self.n_proposals)
-        # Emitter sampling
-        # !!!
-        # we specifically set test_visibility to False
-        # as we would like to skip shadow tracing and save computations
-        # and compensate via RIS
-        # !!!
         active_em = active & mi.has_flag(bsdf.flags(), mi.BSDFFlags.Smooth)
 
         ds, weight_em = scene.sample_emitter_direction(si, sampler.next_2d(), False, active_em)
@@ -85,66 +78,38 @@ class ISIRIntegrator(MyBaseIntegrator):
         reservoir.update(wo, ds.p, bsdf_val_em, weight_em, ds.pdf, active_em)
 
         # Streaming RIS using weighted reservoir sampling
-        for _k in range(self.emitter_samples):
 
-            # we need to sample lights and get sampling weights with corresponding sample
-            # ds.pdf - light sampling pdf based on properties of emitters
-            # weight_em - 3d-valued light function / ds.pdf
-            # proposals = [reservoir.sample]
-            # proposals = [mi.DirectionSample3f()]
-            # weights = [reservoir.weight]
-            weights = [reservoir.weight]
-            # proposals = [ds]
-            wos = [reservoir.sample]
+        for emitter_idx in range(self.emitter_samples):
 
-            i = mi.UInt(0)
-            inner_loop = mi.Loop(name="proposals_generation", state=lambda: (sampler, i, wos, weights))
-            while inner_loop(i < reservoir.n_proposals - 1):
+            #samples_tensor = dr.zeros(dtype=mi.TensorXf,
+            #                          shape=(self.n_proposals, sampler.wavefront_size(), 3))
+            #weights_tensor = dr.zeros(dtype=mi.TensorXf,
+            #                          shape=(self.n_proposals, sampler.wavefront_size(), 3))
+            samples_list = [reservoir.sample]
+            weights_list = [reservoir.weight]
+            #weights_tensor[0, :, :] = np.array(reservoir.weight, dtype=np.float32)
+
+            #samples_tensor[0, :, :] = mi.TensorXf(np.array(reservoir.sample),
+            #                                       shape=[sampler.wavefront_size(), 3])
+
+            #i = mi.UInt(0)
+            #inner_loop = mi.Loop(name="proposals_generation",
+            #                     state=lambda: (sampler, i, samples_tensor, weights_tensor))
+            #while inner_loop(i < reservoir.n_proposals - 1):
+            for jdx in range(self.n_proposals - 1):
                 ds, weight_em = scene.sample_emitter_direction(si, sampler.next_2d(), False, active_em)
-                #print(weight_em.sum_())
                 wo = si.to_local(ds.d)
-                # active_em_ris = active_em
-                # active_em_ris &= dr.neq(ds.pdf, 0.0)
-                # skip samples with zero weights
-                # proposals.append(ds)
-                # print(np.array(weight_em).shape)
-                # print(weight_em.sum_())
-                wos.append(wo)
-                weights.append(weight_em)
+                samples_list.append(wo)
+                weights_list.append(weight_em)
+                #samples_tensor[i+1, :, :] += dr.ones(dtype=mi.TensorXf, shape=[sampler.wavefront_size(), 3])
+                #weights_tensor[i+1, :, :] += dr.ones(dtype=mi.TensorXf, shape=[sampler.wavefront_size(), 3])
+                #i += 1
 
-                i += 1
-
-            # proposals_np = np.stack([x.numpy_() for x in proposals])
-            #for x in weights:
-            #    print(np.array(x).shape)
-            weights_np = np.stack([np.array(x.sum_()) for x in weights], axis=-1)
-            wos_np = [np.array(wo_) for wo_ in wos]
-
-            # weights_np = weights_np / np.sum(weights_np)
-            idx = Categorical_np(np.log(weights_np)).sample()
-            #ds = proposals[idx]
             active_em_ris = active_em
             active_em_ris &= dr.neq(ds.pdf, 0.0)
-            #print(np.array(weights).shape)
-            #print(np.stack(weights).shape)
-            # make thing below work
-            np_weights = np.stack(weights)
-            np_wos = np.stack(wos_np)
-            weight_em = np.take_along_axis(np_weights, np.broadcast_to(idx[np.newaxis, :, np.newaxis], (1,) + np_weights.shape[1:]), axis=0)
-            wo = np.take_along_axis(np_wos, np.broadcast_to(idx[np.newaxis, :, np.newaxis], (1,) + np_wos.shape[1:]), axis=0)
-            # next we evaluate the remaining part of integrated function
-            # i.e. the bsdf part
-            #print(wo.shape)
+
+            wo, weight_em = self.do_isir_resample(weights_list, samples_list)
             bsdf_val_em, bsdf_pdf_em = bsdf.eval_pdf(bsdf_ctx, si, mi.Vector3f(wo), active_em_ris)
-
-            # sampling density p = ds.pdf
-            # integrated function (and desirable unnormalized sampling density) phat= Light * bsdf_val
-            # weight_em = Light / ds.pdf
-            # i.e. resampling weight = phat / p = weight_em * bsdf_val
-
-            # update reservoir based on current sample
-            # as target func as 3d-valued we simply sum over spatial axis
-            # authors do the same, but use weighted sum
 
             reservoir.update(wo, ds.p, bsdf_val_em, mi.Vector3f(weight_em) * bsdf_val_em, ds.pdf, active_em_ris)
 
@@ -165,6 +130,34 @@ class ISIRIntegrator(MyBaseIntegrator):
         L += dr.select(activity_mask, final_emitter_val * final_bsdf_val * reservoir_weight, mi.Color3f(0))
 
         return (L, active, [])
+
+    def do_isir_resample(self,
+                         weights_pool,
+                         samples_pool):
+        if isinstance(weights_pool, list):
+            weights = np.stack([np.array(weight.sum_()) for weight in weights_pool], axis=-1)
+        elif isinstance(weights_pool, mi.TensorXf):
+            weights = np.array(weights_pool).sum(axis=-1)
+        else:
+            weights = weights_pool.sum(axis=-1)
+
+        if isinstance(samples_pool, list):
+            samples = np.stack([np.array(sample) for sample in samples_pool], axis=-1)
+        elif isinstance(samples_pool, mi.TensorXf):
+            samples = np.array(samples_pool)
+        else:
+            samples = samples_pool
+
+        sampling_idx = Categorical_np(np.log(weights)).sample()[np.newaxis, :, np.newaxis]
+
+        lights_pool = np.stack(weights_pool)
+        lights_shape = (1,) + lights_pool.shape[1:]
+        samples_shape = (1,) + samples.shape[1:]
+
+        light_val = np.take_along_axis(lights_pool, np.broadcast_to(sampling_idx, lights_shape), axis=0)
+        sample = np.take_along_axis(samples, np.broadcast_to(sampling_idx, samples_shape), axis=0)
+
+        return sample, light_val
 
 
 if __name__ == '__main__':
